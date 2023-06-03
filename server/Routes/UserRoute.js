1d53fe484d11df05c1e061f63fb7d1662018a65a
balldev1 <balldev1@hotmail.com>
2023-06-03 23:46:48 +0700

    first commit

---
+++ server/Routes/UserRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { UnfollowUser, deleteUser, followUser, getUser, updateUser,getAllUsers } from '../Controllers/UserController.js'
+import authMiddleWare from '../MiddleWare/authMiddleWare.js';
+const router = express.Router();
+
+
+router.get('/', getAllUsers)
+router.get('/:id', getUser)
+router.put('/:id',authMiddleWare, updateUser)
+router.delete('/:id',authMiddleWare, deleteUser)
+router.put('/:id/follow',authMiddleWare, followUser)
+router.put('/:id/unfollow',authMiddleWare, UnfollowUser)
+
+
+export default router
